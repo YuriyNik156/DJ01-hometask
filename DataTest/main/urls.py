13c2c54d715cd6678495f1d09e4948427b972907
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-19 18:43:36 +0300

    Update urls.py

--- DataTest/main/urls.py
+++ DataTest/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('data/', views.data_view, name='data'),
-    path('test/', views.data_view, name='test'),
+    path('', views.data_view, name='data'),
+    path('test/', views.test_view, name='test'),
 ]
